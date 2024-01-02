fbb4e708cf6409f88bc3b2e20620ebdca53e9ae6
Utkarsh Singh <utkarsh.singh@hyperverge.co>
2024-01-02 23:23:55 +0530

    WIP logic for getting curl from the request body in a string

--- server.js
+++ server.js
@@ -6,37 +6,35 @@ const { Configuration, OpenAIApi } = require("openai");
 
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 const port = 5086;
 
-app.post('/lime', async (req, res) => {
-//   const command = req.body.command;
+app.post('/health', (req, res, next) => {
+  res.send('Hello world!' + JSON.stringify(req.body.testProp));
+})
+
+app.post('/test', async (req, res) => {
+  //   const command = req.body.command;
   const text = req.body.text;
   const responseUrl = req.body.response_url;
-  console.log(req.body)
 
-  res.send();
+  const curl = JSON.parse(req.body.command)
+  console.log(curl)
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{
-        "role": "system",
-        "content": `${text}\n\nCan you rephrase the above text into a paragraph for 4-6 lines, in the style of a funny limerick. It should still convey the same message, from the sender to the recipient.`
-    }],
-});
-console.log('result', completion.data.choices[0].message);
-
-  await axios.post(responseUrl, {
-    text: `@${req.body.user_name} says...\n\n${completion.data.choices[0].message.content}`,
-    response_type: "in_channel",
-    as_user: true,
-    user: req.body.user_id
-  })
+  // const completion = await openai.createChatCompletion({
+  //   model: "gpt-3.5-turbo",
+  //   messages: [{
+  //     "role": "system",
+  //     "content": `${text}\n\ This is a curl command and it's response. Can you make an API documentation for this Endpoint. Please include the Headers, HTTP Request Types, Request body, sample request body and Status codes, sample responses for all the cases.`
+  //   }],
+  // });
+  // console.log('result', completion.data.choices[0].message);
 });
 
 app.listen(port, () => {
