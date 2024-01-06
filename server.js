9e6065f49e6109c606d059dfd2ca19e267d506b6
Utkarsh Singh <utkarsh.singh@hyperverge.co>
2024-01-06 19:06:04 +0530

    rearranged a few lines and added uuidv4 for file names

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const axios = require('axios');
-require('dotenv').config();
+const fs = require('fs');
 const { mdToPdf } = require("md-to-pdf");
+const uuid4 = require("uuid4");
 const { Configuration, OpenAIApi } = require("openai");
-const { exec } = require('child_process');
 
+require('dotenv').config();
 
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-// Trust the proxy to handle protocols
+app.use(bodyParser.json());
 app.set('trust proxy', true);
 const port = 5086;
 
-app.all('/makeRequest/*',  async (req, res) => {
-  console.log('request received in /makeRequest')  
+app.all('/createDocs/*',  async (req, res, next) => {
+  console.log('request received in /createDocs')  
   
-  const baseUrl = "https://" + req.url.split("/makeRequest/")[1];
-
+  const baseUrl = "https://" + req.url.split("/createDocs/")[1];
   const requestBody = req.body;
   const requestHeaders = req.headers; 
   delete requestHeaders.host;
@@ -45,11 +40,11 @@ app.all('/makeRequest/*',  async (req, res) => {
       "content": `BaseURL:${JSON.stringify(baseUrl)}\nRequestBody"${JSON.stringify(requestBody)}\nHeaders:${JSON.stringify(requestHeaders)}\nResponse${JSON.stringify(queryResponse)}\nThese are the corresponding data for an API Endpoint. Based on these, make an API Documentation in markdown. Include the way to make the request, parse the important headers and add metadata to the best of your knowledge. Share the sample response and add other types of responses based on your knowledge and understanding of the data. Remove the headers that are commonly not passed by the users. Make the API Documentation as classy looking as possible.`
     }],
   });
-  console.log('result', completion.data.choices[0].message);
 
-  
-  await mdToPdf({content: completion.data.choices[0].message.content}, {dest: 'test.pdf'});
-  res.sendFile( "/Users/utkarsh/projects/docli/test.pdf")
+  const uniqueDocumentationId = uuid4();
+  const fileDestination = `/Users/utkarsh/projects/docli/${uniqueDocumentationId}.pdf` 
+  await mdToPdf({content: completion.data.choices[0].message.content}, {dest:fileDestination});
+  res.sendFile(fileDestination);
 })
 
 app.listen(port, () => {
